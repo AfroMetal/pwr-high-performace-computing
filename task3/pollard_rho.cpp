1e0d771fcce199c5a37f256b9c3f6743f30eef75
afrometal <radek.kwlsk@gmail.com>
2018-03-22 01:25:03 +0100

    proper parallel version according to introduction from https://crypto.stackexchange.com/q/22707: "When using the parallel version of Pollard's Rho algorithm for discrete logs, each processor performs its own random walk to find distinguished points, and reports the starting point and the distinguished point to the server. If two processors report the same distinguished point, then the server can re-do the random walk and find where the collision occurred and solve the discrete logarithm."

--- task3/pollard_rho.cpp
+++ task3/pollard_rho.cpp
@@ -4,20 +4,25 @@
 #include <NTL/ZZ_p.h>
 #include <assert.h>
 #include <tuple>
+#include <unordered_map>
+#include <map>
+#define DIST_BITS 4
 
 using namespace std;
 using namespace NTL;
 
+
 ZZ pollard_rho(ZZ alpha, ZZ beta, ZZ P) {
     int tid;
-    ZZ x, a, b, X, A, B, res, r;
+    ZZ x, a, b, a0, b0, X, A, B, res, r;
     bool quit = false;
     ZZ Q = (P - ZZ(1)) / ZZ(2);
+    map<ZZ, pair<ZZ, ZZ>> distinguished_values;
 
     #pragma omp parallel \
-    num_threads(2) \
-    shared(res, quit) \
-    private(tid, x, a, b, X, A, B, r) \
+    num_threads(4) \
+    shared(res, quit, distinguished_values) \
+    private(tid, x, a, b, r, a0, b0, A, B, X) \
     firstprivate(alpha, beta, P, Q)
     {
         tid = omp_get_thread_num();
@@ -30,103 +35,119 @@ ZZ pollard_rho(ZZ alpha, ZZ beta, ZZ P) {
             do {
                 b = RandomBnd(Q);
             } while (b == 0);
+            a0 = a;
+            b0 = b;
             x = MulMod(PowerMod(alpha, a, P), PowerMod(beta, b, P), P);
-
-            X = x;
-            A = a;
-            B = b;
         }
 
         #pragma omp barrier
 
-//#pragma omp critical
-//        {
-//            cout << tid << ": (" << x << ", " << X <<  ")\n";
-//            cout << tid << ": quit = " << quit <<  "\n";
-//            cout << tid << ": res = " << res <<  "\n\n";
-//        };
-
-//        #pragma omp barrier
-
         do {
-            #pragma omp critical
-            {
-                switch (x % 3) {
-                    case 1: /* S1 */
-                        MulMod(x, x, beta, P);
-                        // a = a;
-                        AddMod(b, b, 1, Q);
-                        break;
-                    case 0: /* S2 */
-                        MulMod(x, x, x, P);
-                        MulMod(a, a, 2, Q);
-                        MulMod(b, b, 2, Q);
-                        break;
-                    case 2: /* S3 */
-                        MulMod(x, x, alpha, P);
-                        AddMod(a, a, 1, Q);
-                        // b = b;
-                        break;
-                    default:
-                        exit(1);
-                }
-                switch (X % 3) {
-                    case 1: /* S1 */
-                        MulMod(X, X, beta, P);
-                        // A = A;
-                        AddMod(B, B, 1, Q);
-                        break;
-                    case 0: /* S2 */
-                        MulMod(X, X, X, P);
-                        MulMod(A, A, 2, Q);
-                        MulMod(B, B, 2, Q);
-                        break;
-                    case 2: /* S3 */
-                        MulMod(X, X, alpha, P);
-                        AddMod(A, A, 1, Q);
-                        // B = B;
-                        break;
-                    default:
-                        exit(1);
-                }
-                switch (X % 3) {
-                    case 1: /* S1 */
-                        MulMod(X, X, beta, P);
-                        // A = A;
-                        AddMod(B, B, 1, Q);
-                        break;
-                    case 0: /* S2 */
-                        MulMod(X, X, X, P);
-                        MulMod(A, A, 2, Q);
-                        MulMod(B, B, 2, Q);
-                        break;
-                    case 2: /* S3 */
-                        MulMod(X, X, alpha, P);
-                        AddMod(A, A, 1, Q);
-                        // B = B;
-                        break;
-                    default:
-                        exit(1);
+            switch (x % 3) {
+                case 1: /* S1 */
+                    MulMod(x, x, beta, P);
+                    // a = a;
+                    AddMod(b, b, 1, Q);
+                    break;
+                case 0: /* S2 */
+                    MulMod(x, x, x, P);
+                    MulMod(a, a, 2, Q);
+                    MulMod(b, b, 2, Q);
+                    break;
+                case 2: /* S3 */
+                    MulMod(x, x, alpha, P);
+                    AddMod(a, a, 1, Q);
+                    // b = b;
+                    break;
+                default:
+                    exit(1);
+            }
+
+            if ((x & ZZ((1 << DIST_BITS) - 1)) == 0) {
+                #pragma omp critical
+                if (!quit) {
+                    pair<map<ZZ, pair<ZZ, ZZ>>::iterator, bool> ret = distinguished_values.insert(
+                            pair<ZZ, pair<ZZ, ZZ>>(x, pair<ZZ, ZZ>(a0, b0)));
+                    if (!ret.second) {
+                        quit = true;
+                        a = ret.first->second.first;
+                        b = ret.first->second.second;
+                        x = MulMod(PowerMod(alpha, a, P), PowerMod(beta, b, P), P);
+                        X = x;
+                        A = a;
+                        B = b;
+
+                        do {
+                            switch (x % 3) {
+                                case 1: /* S1 */
+                                    MulMod(x, x, beta, P);
+                                    // a = a;
+                                    AddMod(b, b, 1, Q);
+                                    break;
+                                case 0: /* S2 */
+                                    MulMod(x, x, x, P);
+                                    MulMod(a, a, 2, Q);
+                                    MulMod(b, b, 2, Q);
+                                    break;
+                                case 2: /* S3 */
+                                    MulMod(x, x, alpha, P);
+                                    AddMod(a, a, 1, Q);
+                                    // b = b;
+                                    break;
+                                default:
+                                    exit(1);
+                            }
+                            switch (X % 3) {
+                                case 1: /* S1 */
+                                    MulMod(X, X, beta, P);
+                                    // A = A;
+                                    AddMod(B, B, 1, Q);
+                                    break;
+                                case 0: /* S2 */
+                                    MulMod(X, X, X, P);
+                                    MulMod(A, A, 2, Q);
+                                    MulMod(B, B, 2, Q);
+                                    break;
+                                case 2: /* S3 */
+                                    MulMod(X, X, alpha, P);
+                                    AddMod(A, A, 1, Q);
+                                    // B = B;
+                                    break;
+                                default:
+                                    exit(1);
+                            }
+                            switch (X % 3) {
+                                case 1: /* S1 */
+                                    MulMod(X, X, beta, P);
+                                    // A = A;
+                                    AddMod(B, B, 1, Q);
+                                    break;
+                                case 0: /* S2 */
+                                    MulMod(X, X, X, P);
+                                    MulMod(A, A, 2, Q);
+                                    MulMod(B, B, 2, Q);
+                                    break;
+                                case 2: /* S3 */
+                                    MulMod(X, X, alpha, P);
+                                    AddMod(A, A, 1, Q);
+                                    // B = B;
+                                    break;
+                                default:
+                                    exit(1);
+                            }
+                        } while(x != X);
+
+                        SubMod(r, b, B, Q);
+                        if (r == 0) {
+                            res = ZZ(-1);
+                        } else {
+                            InvMod(r, r, Q);
+                            MulMod(res, r, SubMod(A, a, Q), Q);
+                        }
+                    }
                 }
             }
-        } while (x != X && !quit);
-
-        #pragma omp single
-        quit = true;
-
-        #pragma omp critical
-        if (x == X) {
-            SubMod(r, b, B, Q);
-//            cout << "r = " << r << '\n';
-            if (r == 0) {
-                res = ZZ(-1);
-            } else {
-                InvMod(r, r, Q);
-//                cout << tid << ": inv_r = " << inv_r << '\n';
-                MulMod(res, r, SubMod(A, a, Q), Q);\
-//            cout << tid << ": result = " << res << "\n\n";
-            }
-        }
+        } while (!quit);
     }
 
     return res;
@@ -138,10 +159,15 @@ int main(int argc, char **argv) {
     ZZ alpha;
     ZZ beta;
 
-    // 40 bits
-    P = 971579802563; // P
-    alpha = 310118604200; // generator g
-    beta = 93968352314; // y = alpha^{x} = beta (mod P)
+//    // 40 bits
+//    P = 971579802563; // P
+//    alpha = 310118604200; // generator g
+//    beta = 93968352314; //
+
+    // 45 bits
+    P = 30034629688907; // P
+    alpha = 15963176245506; // generator g
+    beta = 13006361946477; // y = alpha^{x} = beta (mod P)
 
 //    // 50 bits
 //    P = 1869034281506423; // P
