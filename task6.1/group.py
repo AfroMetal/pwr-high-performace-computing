8925c42a463be524379743e4e38715687f8e7cf6
Radosław Kowalski <radek.kwlsk@gmail.com>
2018-06-04 19:32:48 +0200

    list 6 task 1 helper script

---
+++ task6.1/group.py
@@ -0,0 +1,26 @@
+N = 2584
+r = 1152
+
+
+def gcd(u, v):
+    while v:
+        u, v = v, u % v
+    return abs(u)
+
+
+def group_elements():
+    for i in range(1, N):
+        if gcd(i, N) == 1:
+            yield i
+
+
+if __name__ == "__main__":
+    i = 0
+    for a in group_elements():
+        i += 1
+        try:
+            assert a ** r % N == 1
+        except AssertionError:
+            print(f"{a}^r != 1 mod {N}")
+
+    print(f"{i} group elements checked!")
